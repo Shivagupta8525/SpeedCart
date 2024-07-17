005b5fef61493252f8c49cd45c3b95d05e12ee2e
shivagupta8525 <shivagupta8525@gmail.com>
2024-07-17 22:33:31 +0530

    cart row color change

--- src/Component/cartProductDetails.jsx
+++ src/Component/cartProductDetails.jsx
@@ -1,45 +1,4 @@
-//  import React, { useEffect, useState } from "react";
-//  import { Link } from "react-router-dom";
-//  import { getProData } from "./api";
-//  function cartProductDetails({ id, quantity }) {
-//      const [product, setProduct] = useState();
-//      useEffect(function () {
-//          getProData(id).then(function (data) {
-//              setProduct(data)
-//          })
-//      }, [])
-//      if (!product) {
-//          return <></>
-//      }
-
-//      return (
-//          // <>
-//          //  <div className="flex flex-col gap-2 sm:flex-row sm:justify-evenly  sm:mr-0 sm:py-2"> 
-//          //      <img className="border max-w-16 max-h-16 " src={product.thumbnail} />
-//          //      <h2 className="self-center bold text-xl text-orange-600">{product.title}</h2>
-//          //      <h2 className="self-center bold text-xl">{product.price}</h2>
-//          //      <p className="border px-6 max-h-10 self-center py-2">{quantity}</p>
-//          //      <h2 className="self-center">${product.price*quantity}</h2>
-//          //  </div>
-//          //  <hr className=""/>
-//          //  </>
-//          <tr>
-//              <td className="border-b p-2 flex items-center">
-//                  <img src={product.thumbnail} alt={product.name} className="h-16 w-16 object-cover mr-4" />
-//                  <span>{product.title}</span>
-//                  {/* <button onClick={() => onRemoveItem(product.id)} className="ml-4 text-red-500">X</button> */}
-//              </td>
-//              <td className="border-b p-2">${product.price}</td>
-//              <td className="border-b p-2">${quantity}
-
-//              </td>
-//              <td className="border-b p-2">${(product.price *quantity.toFixed(2)) }</td>
-//          </tr>
-
-
-//      );
-//  }
-//  export default cartProductDetails;
+ 
 
  import React, { useEffect, useState } from "react";
  import { Link } from "react-router-dom";
@@ -55,24 +14,14 @@
          return <></>
      }
 
-     return (
-         // <>
-         //  <div className="flex flex-col gap-2 sm:flex-row sm:justify-evenly  sm:mr-0 sm:py-2"> 
-         //      <img className="border max-w-16 max-h-16 " src={product.thumbnail} />
-         //      <h2 className="self-center bold text-xl text-orange-600">{product.title}</h2>
-         //      <h2 className="self-center bold text-xl">{product.price}</h2>
-         //      <p className="border px-6 max-h-10 self-center py-2">{quantity}</p>
-         //      <h2 className="self-center">${product.price*quantity}</h2>
-         //  </div>
-         //  <hr className=""/>
-         //  </>
-         <tr className="bg-red-100   w-screen flex-col">
+     return ( 
+         <tr className="  w-screen flex-col">
              <td className="border-b p-2  flex  items-center justify-around">
                  <img src={product.thumbnail} alt={product.name} className="h-16 w-16 object-cover mr-4" />
                  <span>{product.title}</span>
                  {/* <button onClick={() => onRemoveItem(product.id)} className="ml-4 text-red-500">X</button> */}
              </td>
-             <td className="border-b p-2 text-center bg-blue-200 ">${product.price}</td>
+             <td className="border-b p-2 text-center  ">${product.price}</td>
              <td className="border-b p-2 text-center">${quantity}
 
              </td>
